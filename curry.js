aea20aed13efeef3317c14a9e41dc19a09c88d1a
totihuang <v_totihuang@tencent.com>
2018-08-20 14:42:51 +0800

    [+]增加 curry 函数的研究

---
+++ curry.js
@@ -0,0 +1,35 @@
+/**
+ * 函数 curry 化研究
+ */
+export default function () {
+    function add(a, b, c) {
+        return a + b + c;
+    }
+
+    /** 带预传值的 curry */
+    var curry1 = (fn, ...args) => {
+        return function (..._args) {
+            return fn.apply(null, [...args, ..._args]);
+        };
+    };
+
+    /** 带参数调用变化的 curry */
+    function curry2(fn, length) {
+        length = length || fn.length;
+
+        return function (..._args) {
+            if (_args.length < length) {
+                return curry2(
+                    curry1.apply(this, [fn, ..._args]),
+                    length - _args.length
+                );
+            } else {
+                return fn.apply(this, _args);
+            }
+        };
+    }
+
+    var sum = curry2(add);
+
+    console.log(sum(2, 2)(2));
+}
