540a513b79d0f875ea98d41c0b52dbf0080be4cd
ThatToti <mr.totihuang@gmail.com>
2018-08-20 15:20:04 +0800

    [+]配置 codesanbox

--- byteTransfer.js
+++ byteTransfer.js
@@ -1,32 +1,22 @@
+/**
+ * 字节转换函数
+ */
+export function byteTransfer(byte) {
+  var k = 1024;
 
+  var sizes = ["B", "K", "M", "G", "T"];
 
+  var i = 0;
 
+  var byteTransfered;
 
+  if (byte === 0) {
+    i = 0;
+  } else {
+    i = Math.floor(Math.log(byte) / Math.log(k));
+  }
 
-function byteTransfer(byte) {
+  byteTransfered = byte / Math.pow(k, i).toFixed(1) + sizes[i];
 
-    var k = 1024;
-
-    var sizes = ['B', 'K', 'M', 'G', 'T'];
-
-    var i = 0;
-
-    var byteTransfered;
-
-    if (byte === 0) {
-
-        i=0;
-
-    }else{
-
-        i=Math.floor(Math.log(byte)/Math.log(k));
-
-    }
-
-    byteTransfered = byte/Math.pow(k,i).toFixed(1)+sizes[i];
-
-    return byteTransfered;
-
+  return byteTransfered;
 }
-
-console.log(byteTransfer(1024));
---
+++ index.js
@@ -0,0 +1,107 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+/** 触摸事件 */
+// import touch from "./touch";
+/** 函数 curry 化 */
+// import curry from "./curry";
+// import xss from "./xss";
+// import senior from "./senior";
+// import animate  from './animate'
+// import Event from './eventTarget'
+// import lazycount from "./lazycount";
+
+// touch();
+// curry();
+// senior();
+// animate()
+// Event()
+// lazycount();
+
+// var vconsole = new VConsole();
+// xss();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.wrap = React.createRef();
+  }
+
+  componentDidMount() {
+    this.wrap.current.addEventListener(
+      "touchmove",
+      evt => {
+        // evt.preventDefault();
+        evt.stopPropagation();
+      },
+      {
+        passive: false
+      }
+    );
+
+    var start = null;
+    var element = this.wrap.current;
+    element.style.position = "absolute";
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      var progress = timestamp - start;
+      element.style.left = Math.min(progress / 10, 200) + "px";
+      if (progress < 2000) {
+        window.requestAnimationFrame(step);
+      }
+    }
+
+    // window.requestAnimationFrame(step);
+  }
+  render() {
+    return (
+      <div className="App" onTouchStart={evt => this.touchStart(evt)}>
+        <div
+          className="inside"
+          ref={this.wrap}
+          id="SomeElementYouWantToAnimate"
+        >
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello toti</h1>
+          <h1>Hello CodeSandtoti</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello CodeSandbox</h1>
+          <h1>Hello toti</h1>
+        </div>
+      </div>
+    );
+  }
+
+  touchStart(evt) {}
+
+  touchMove(evt) {}
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
